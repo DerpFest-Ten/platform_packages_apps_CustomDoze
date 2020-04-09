2adb0ba2f1715f8f2d5751f259b6d55af9465b1c
Nauval Rizky <enuma.alrizky@gmail.com>
2020-04-09 11:37:44 +0200

    CustomDoze: implement raise to wake (2/2)
    * Highly inspired by iOS raise to wake
    
    Adapted for BlissRom by: Pietro Ivan Cerrato <ivan@cerrato.biz>
    
    Change-Id: Ide51d12fc97c840c216bcb1b0321248f9d695cd9
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/custom/ambient/display/DozeSettings.java
+++ src/com/custom/ambient/display/DozeSettings.java
@@ -72,6 +72,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
         private SwitchPreference mDozeOnChargePreference;
         private SwitchPreference mAmbientDisplayPreference;
         private SwitchPreference mPickUpPreference;
+        private SwitchPreference mRaiseToWakePreference;
         private SwitchPreference mHandwavePreference;
         private SwitchPreference mPocketPreference;
 
@@ -110,6 +111,10 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             mPickUpPreference.setChecked(Utils.tiltGestureEnabled(mContext));
             mPickUpPreference.setOnPreferenceChangeListener(this);
 
+	    mRaiseToWakePreference = (SwitchPreference) findPreference(Utils.GESTURE_RAISE_TO_WAKE_KEY);
+            mRaiseToWakePreference.setChecked(Utils.raiseToWakeGestureEnabled(mContext));
+            mRaiseToWakePreference.setOnPreferenceChangeListener(this);
+
             mHandwavePreference =
                 (SwitchPreference) findPreference(Utils.GESTURE_HAND_WAVE_KEY);
             mHandwavePreference.setChecked(Utils.handwaveGestureEnabled(mContext));
@@ -124,6 +129,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             if (!getResources().getBoolean(R.bool.has_tilt_sensor)) {
                 getPreferenceScreen().removePreference(mTiltCategory);
                 getPreferenceScreen().removePreference(mPickUpPreference);
+                getPreferenceScreen().removePreference(mRaiseToWakePreference);
             }
 
             mProximitySensorCategory =
@@ -160,6 +166,10 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
                 mPickUpPreference.setChecked(value);
                 Utils.enablePickUp(value, mContext);
                 return true;
+            } else if (Utils.GESTURE_RAISE_TO_WAKE_KEY.equals(key)) {
+                mRaiseToWakePreference.setChecked(value);
+                Utils.enableRaiseToWake(value, mContext);
+                return true;
             } else if (Utils.GESTURE_HAND_WAVE_KEY.equals(key)) {
                 mHandwavePreference.setChecked(value);
                 Utils.enableHandWave(value, mContext);
@@ -177,6 +187,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             mAmbientDisplayPreference.setEnabled(!aodEnabled);
             mDozeOnChargePreference.setEnabled(!aodEnabled);
             mPickUpPreference.setEnabled(!aodEnabled);
+            mRaiseToWakePreference.setEnabled(!aodEnabled);
             mHandwavePreference.setEnabled(!aodEnabled);
             mPocketPreference.setEnabled(!aodEnabled);
         }
